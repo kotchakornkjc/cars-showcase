60550482ba19981d3d4381cf0ee7a2c89a14cf7e
kotchakornkjc <kotchakorn.kjc@gmail.com>
2023-08-18 15:43:43 +0700

    update env

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    NEXT_PUBLIC_G_KEY: process.env.NEXT_PUBLIC_RAPID_API_KEY,
+    NEXT_PUBLIC_IMAGE_API_KEY: process.env.NEXT_PUBLIC_IMAGE_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
